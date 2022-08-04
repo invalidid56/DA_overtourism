3c100c24b9fe7925ac1c91a6f408cf3c41ced941
invalidid56 <karl2003@naver.com>
2022-08-05 00:02:12 +0900

    end

--- data.py
+++ data.py
@@ -1,8 +1,5 @@
-import pandas as pd
+from konlpy.tag import Okt, Komoran
 
-
-df = pd.read_csv(
-    '2018.csv',
-    encoding='euc-kr'
-)
-print(df)
+tagger = Komoran(userdic='userdic.txt')
+print(tagger.pos('플로깅이 유행이다'))
+print(tagger.nouns('플로깅이 유행이다'))
